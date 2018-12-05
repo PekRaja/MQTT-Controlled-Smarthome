b4be799c63781b45fb94cea30b121abb712151d4
PekRaja <44567961+PekRaja@users.noreply.github.com>
2018-12-05 13:36:56 +0200

    Update LDR.ino

--- Huzzah codes/LDR/LDR.ino
+++ Huzzah codes/LDR/LDR.ino
@@ -1,3 +1,29 @@
+ /***************************************************
+  Adafruit MQTT Library ESP8266 Example
+
+  Must use ESP8266 Arduino from:
+    https://github.com/esp8266/Arduino
+
+  Works great with Adafruit's Huzzah ESP board:
+  ----> https://www.adafruit.com/product/2471
+
+  Adafruit invests time and resources providing this open source code,
+  please support Adafruit and open-source hardware by purchasing
+  products from Adafruit!
+
+  Written by Tony DiCola for Adafruit Industries.
+  MIT license, all text above must be included in any redistribution
+ ****************************************************/
+
+ /*
+ This is the code for a LDR controlled LED
+ Both the LDR and the LED can be toggled via MQTT (a Raspberry Pi was used as the broker and WiFi hotspot)
+ The LED is connected to pin 16 and the LDR to analog pin 0 (beware that Huzzah analog pin can only take maximum on 1V)
+ Code is built based on an example by Tony DiCola mentioned above
+ Project used this version of Huzzah https://www.adafruit.com/product/2821
+ Made by Pekka Rajala, Aki Seppänen, Eemeli Tikka from Oulu University of Applied Sciences (second year students at the time)
+ as part of a larger project for a remote controlled Smarthome.
+ */
 /*****************************Libraries**************************************/
 #include <ESP8266WiFi.h>
 #include "Adafruit_MQTT.h"
@@ -5,20 +31,21 @@
 
 /************************* WiFi Access Point *********************************/
 
-#define WLAN_SSID       "Raspi"
-#define WLAN_PASS       "Ytrewq123"
+#define WLAN_SSID       "*******"
+#define WLAN_PASS       "********"
 
 
 /************************* Adafruit.io Setup *********************************/
 
-#define AIO_SERVER      "172.20.248.132"
+#define AIO_SERVER      "***********"    //Broker IP or address
 #define AIO_SERVERPORT  1883
-#define AIO_USERNAME    "raspi"
-#define AIO_KEY         "Ytrewq"
-#define AIO_CLIENTID    "LEDS"
+#define AIO_USERNAME    "***********"    //Username that has been set up in your broker
+#define AIO_KEY         "***********"    //Password for the above username in your broker
+#define AIO_CLIENTID    "***********"    //Device id, this needs to be unique for each of your devices connected to your broker
 
+/*********************** Define pins *****************************************/
 #define BLUE_LED 2
-#define H1 16       //Room 1 ie. Livingroom
+#define H1 16                             //Room 1 ie. Livingroom
 #define LDR A0
 
 /************ Global State (you don't need to change this!) ******************/
@@ -30,7 +57,6 @@ WiFiClient client;
 Adafruit_MQTT_Client mqtt(&client, AIO_SERVER, AIO_SERVERPORT, AIO_CLIENTID, AIO_USERNAME, AIO_KEY);
 
 /****************************** Feeds ***************************************/
-
 
 // Setup a feeds called h1 and ldr
 Adafruit_MQTT_Subscribe h1 = Adafruit_MQTT_Subscribe(&mqtt, "h1");
@@ -154,7 +180,7 @@ void loop()
     mqtt.disconnect();
   }
   
-/***************************/
+// LDR read only if it is set on
   if (Auto == true)
   {
     Ldr();
