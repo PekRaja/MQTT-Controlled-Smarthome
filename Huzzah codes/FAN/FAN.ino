9aaa14c5216fa860d4623d9e6e1fa356a8fbe6e1
PekRaja <44567961+PekRaja@users.noreply.github.com>
2018-12-05 08:58:34 +0200

    Mqtt controlled Smarthome
    aaaa

---
+++ Huzzah codes/FAN/FAN.ino
@@ -0,0 +1,169 @@
+ /***************************************************
+  Adafruit MQTT Library ESP8266 Example
+
+  Must use ESP8266 Arduino from:
+    https://github.com/esp8266/Arduino
+
+  Works great with Adafruit's Huzzah ESP board:
+  ----> https://www.adafruit.com/product/2471
+
+  Adafruit invests time and resources providing this open source code,
+  please support Adafruit and open-source hardware by purchasing
+  products from Adafruit!
+
+  Written by Tony DiCola for Adafruit Industries.
+  MIT license, all text above must be included in any redistribution
+ ****************************************************/
+
+ /*
+ This is the code for MQTT-message controlled fan.
+ The fan is powered by an external power-soource and controlled by Huzzah! pin 15 via FET
+ Code is built based on an example by Tony DiCola mentioned above
+ Project used this version of Huzzah https://www.adafruit.com/product/2821
+ Made by Pekka Rajala, Aki Seppänen, Eemeli Tikka from Oulu University of Applied Sciences (second year students at the time)
+ as part of a larger project for a remote controlled Smarthome.
+ */
+
+#include <ESP8266WiFi.h>
+#include "Adafruit_MQTT.h"
+#include "Adafruit_MQTT_Client.h"
+
+/************************* WiFi Access Point *********************************/
+
+#define WLAN_SSID       "Raspi"
+#define WLAN_PASS       "Ytrewq123"
+
+/************************* Adafruit.io Setup *********************************/
+
+#define AIO_SERVER      "172.20.248.132" //Broker IP or address
+#define AIO_SERVERPORT  1883
+#define AIO_USERNAME    "raspi"          //Username that has been set up in your broker
+#define AIO_KEY         "Ytrewq"         //Password for the above username in your broker
+#define AIO_CLIENTID    "Fan"            //Device id, this needs to be unique for each of your devices connected to your broker
+
+#define FAN 15
+
+/************ Global State (you don't need to change this!) ******************/
+
+// Create an ESP8266 WiFiClient class to connect to the MQTT server.
+WiFiClient client;
+
+// Setup the MQTT client class by passing in the WiFi client and MQTT server and login details.
+Adafruit_MQTT_Client mqtt(&client, AIO_SERVER, AIO_SERVERPORT, AIO_CLIENTID, AIO_USERNAME, AIO_KEY);
+
+/****************************** Feeds ***************************************/
+
+
+// Setup a feed called 'fan' this is needed to be subscribed to a topic called fan
+Adafruit_MQTT_Subscribe fan = Adafruit_MQTT_Subscribe(&mqtt, "fan");
+
+// Setup a topic called info to publish messages to
+Adafruit_MQTT_Publish sendPub = Adafruit_MQTT_Publish(&mqtt, "info");
+
+// Set messages to be published in a topic
+uint8_t setFan[] = "Fan On";
+uint8_t clearFan[] = "Fan Off";
+
+/*************************** Sketch Code ************************************/
+
+void fancallback(double x) 
+{
+  if(x > 0)
+  {
+    digitalWrite(FAN,HIGH);
+    sendPub.publish(setFan, sizeof(setFan));
+    /*
+    if (! sendPub.publish(setFan, sizeof(setFan)))
+        Serial.println(F("Publish Failed."));
+    else {
+        Serial.println(F("Publish Success!"));
+    }*/
+  }
+  else
+  {
+    digitalWrite(FAN,LOW);
+    sendPub.publish(clearFan, sizeof(clearFan));
+    /*
+    if (! sendPub.publish(clearFan, sizeof(clearFan)))
+        Serial.println(F("Publish Failed."));
+    else 
+    {
+        Serial.println(F("Publish Success!"));
+    }*/
+  }
+}
+
+void setup() {
+  Serial.begin(115200);
+  delay(10);
+
+  // Connect to WiFi access point.
+  Serial.println(); Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(WLAN_SSID);
+
+  WiFi.begin(WLAN_SSID, WLAN_PASS);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println();
+
+  Serial.println("WiFi connected");
+  Serial.println("IP address: "); Serial.println(WiFi.localIP());
+
+  pinMode(FAN,OUTPUT);
+
+  digitalWrite(FAN,LOW);
+
+  fan.setCallback(fancallback);
+
+  // Setup MQTT subscription for fan.
+  mqtt.subscribe(&fan);
+}
+
+uint32_t x=0;
+
+void loop() {
+  // Ensure the connection to the MQTT server is alive (this will make the first
+  // connection and automatically reconnect when disconnected).  See the MQTT_connect
+  // function definition further below.
+  MQTT_connect();
+
+  // this is our 'wait for incoming subscription packets and callback em' busy subloop
+  // try to spend your time here:
+  mqtt.processPackets(10000);
+  
+  // ping the server to keep the mqtt connection alive
+  // NOT required if you are publishing once every KEEPALIVE seconds
+  
+  if(! mqtt.ping()) {
+    mqtt.disconnect();
+  }
+}
+
+// Function to connect and reconnect as necessary to the MQTT server.
+// Should be called in the loop function and it will take care if connecting.
+void MQTT_connect() 
+{
+  int8_t ret;
+  // Stop if already connected.
+  if (mqtt.connected()) {
+    return;
+  }
+  Serial.print("Connecting to MQTT... ");
+
+  uint8_t retries = 3;
+  while ((ret = mqtt.connect()) != 0) { // connect will return 0 for connected
+       Serial.println(mqtt.connectErrorString(ret));
+       Serial.println("Retrying MQTT connection in 10 seconds...");
+       mqtt.disconnect();
+       delay(10000);  // wait 10 seconds
+       retries--;
+       if (retries == 0) {
+         // basically die and wait for WDT to reset me
+         while (1);
+       }
+  }
+  Serial.println("MQTT Connected!");
+}
---
+++ Huzzah codes/LDR/LDR.ino
@@ -0,0 +1,188 @@
+/*****************************Libraries**************************************/
+#include <ESP8266WiFi.h>
+#include "Adafruit_MQTT.h"
+#include "Adafruit_MQTT_Client.h"
+
+/************************* WiFi Access Point *********************************/
+
+#define WLAN_SSID       "Raspi"
+#define WLAN_PASS       "Ytrewq123"
+
+
+/************************* Adafruit.io Setup *********************************/
+
+#define AIO_SERVER      "172.20.248.132"
+#define AIO_SERVERPORT  1883
+#define AIO_USERNAME    "raspi"
+#define AIO_KEY         "Ytrewq"
+#define AIO_CLIENTID    "LEDS"
+
+#define BLUE_LED 2
+#define H1 16       //Room 1 ie. Livingroom
+#define LDR A0
+
+/************ Global State (you don't need to change this!) ******************/
+
+// Create an ESP8266 WiFiClient class to connect to the MQTT server.
+WiFiClient client;
+
+// Setup the MQTT client class by passing in the WiFi client and MQTT server and login details.
+Adafruit_MQTT_Client mqtt(&client, AIO_SERVER, AIO_SERVERPORT, AIO_CLIENTID, AIO_USERNAME, AIO_KEY);
+
+/****************************** Feeds ***************************************/
+
+
+// Setup a feeds called h1 and ldr
+Adafruit_MQTT_Subscribe h1 = Adafruit_MQTT_Subscribe(&mqtt, "h1");
+Adafruit_MQTT_Subscribe ldr = Adafruit_MQTT_Subscribe(&mqtt, "ldr");
+
+// Setup functions to publish messages to topic info and h1
+Adafruit_MQTT_Publish sendPub = Adafruit_MQTT_Publish(&mqtt, "info");
+Adafruit_MQTT_Publish sendh1 = Adafruit_MQTT_Publish(&mqtt, "h1");
+
+// Setup messages to be sent (these are used in info topic)
+uint8_t setH1[] = "Livingroom Light On";
+uint8_t setLDR[] = "LDR On";
+uint8_t clearH1[] = "Livingroom Light Off";
+uint8_t clearLDR[] = "LDR Off";
+
+int ldrval = 0;
+int h1state = LOW;
+bool Auto = false;
+/*************************** Sketch Code ************************************/
+void Ldr()
+{
+  ldrval = analogRead(LDR);
+  if(ldrval < 600 && h1state == LOW)
+  {
+    sendh1.publish(1);
+  }
+  else if(ldrval > 650 && h1state == HIGH)
+  {
+    sendh1.publish(0);
+  }
+  else
+  {
+    return;
+  }
+}
+
+void h1callback(double x) {
+  if(x > 0)
+  {
+    digitalWrite(H1,HIGH);
+    h1state = HIGH;
+    sendPub.publish(setH1, sizeof(setH1));
+  }
+  else{
+    digitalWrite(H1,LOW);
+    h1state = LOW;
+    sendPub.publish(clearH1, sizeof(clearH1));
+  }
+}
+void ldrcallback(double x) 
+{
+  if(x > 0)
+  {
+    Auto = true;
+    digitalWrite(BLUE_LED,LOW);
+    sendPub.publish(setLDR, sizeof(setLDR));
+  }
+  else
+  {
+    Auto = false;
+    digitalWrite(BLUE_LED,HIGH);
+    sendPub.publish(clearLDR, sizeof(clearLDR));
+  }
+}
+
+void setup() 
+{
+  Serial.begin(115200);
+  delay(10);
+
+/*******************Wifi connection*********************/
+
+  // Connect to WiFi access point.
+  Serial.println(); Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(WLAN_SSID);
+
+  WiFi.begin(WLAN_SSID, WLAN_PASS);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println();
+
+  Serial.println("WiFi connected");
+  Serial.println("IP address: "); Serial.println(WiFi.localIP());
+  
+
+  pinMode(BLUE_LED,OUTPUT);
+  pinMode(H1,OUTPUT);
+  digitalWrite(H1,LOW);
+  digitalWrite(BLUE_LED,HIGH);
+  
+/*******************Pin declaring*********************/
+  
+  h1.setCallback(h1callback);
+  ldr.setCallback(ldrcallback);  
+  // Setup MQTT subscription for h1 and ldr.
+  mqtt.subscribe(&h1);
+  mqtt.subscribe(&ldr);
+}
+
+uint32_t x=0;
+
+void loop() 
+{
+/***MQTT function calls*****/
+  // Ensure the connection to the MQTT server is alive (this will make the first
+  // connection and automatically reconnect when disconnected).  See the MQTT_connect
+  // function definition further below.
+  MQTT_connect();
+
+  // this is our 'wait for incoming subscription packets and callback em' busy subloop
+  // try to spend your time here:
+  mqtt.processPackets(10000);
+  
+  // ping the server to keep the mqtt connection alive
+  // NOT required if you are publishing once every KEEPALIVE seconds
+  
+  if(! mqtt.ping()) {
+    mqtt.disconnect();
+  }
+  
+/***************************/
+  if (Auto == true)
+  {
+    Ldr();
+  } 
+}
+
+// Function to connect and reconnect as necessary to the MQTT server.
+// Should be called in the loop function and it will take care if connecting.
+void MQTT_connect() 
+{
+  int8_t ret;
+  // Stop if already connected.
+  if (mqtt.connected()) {
+    return;
+  }
+  Serial.print("Connecting to MQTT... ");
+
+  uint8_t retries = 3;
+  while ((ret = mqtt.connect()) != 0) { // connect will return 0 for connected
+       Serial.println(mqtt.connectErrorString(ret));
+       Serial.println("Retrying MQTT connection in 10 seconds...");
+       mqtt.disconnect();
+       delay(10000);  // wait 10 seconds
+       retries--;
+       if (retries == 0) {
+         // basically die and wait for WDT to reset me
+         while (1);
+       }
+  }
+  Serial.println("MQTT Connected!");
+}
